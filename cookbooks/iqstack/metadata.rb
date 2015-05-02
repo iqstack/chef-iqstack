9bf7783f2802da89f78f1b37442db650e435f351
Chris Jones <cjones@cloudm2.com>
2015-05-02 15:00:25 -0400

    Init

---
+++ cookbooks/iqstack/metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-iqstack'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures chef-iqstack'
+long_description 'Installs/Configures chef-iqstack'
+version          '0.1.0'
+
---
+++ cookbooks/iqstack/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef-iqstack
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
